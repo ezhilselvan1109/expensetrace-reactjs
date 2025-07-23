d5a1fdbb65f6359ff4a8df5e6cd1afefc96cb672
ezhilselvan1109 <ezhilselvan1109@gmail.com>
2025-07-23 22:52:39 +0530

    some page responsive design

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -5,7 +5,9 @@ export default function LoadingSpinner() {
       <div className="text-center">
         <div className="flex justify-center mb-4">
           <div className="relative">
-            <div className="absolute inset-0 h-12 w-12 border-2 border-indigo-200 border-t-indigo-600 rounded-full animate-spin"></div>
+            <div className="absolute inset-0 h-12 w-12 border-2 border-indigo-200 border-t-indigo-600 rounded-full animate-spin">
+              <div>Et</div>
+            </div>
           </div>
         </div>
         <div className="text-lg font-semibold text-gray-900 mb-2">ExpenseTrace</div>
